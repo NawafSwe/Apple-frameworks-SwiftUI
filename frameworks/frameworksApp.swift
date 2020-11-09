cecd52488fda30fa2462b27755eba25fd2e9cf2a
NawafSwe <nawaf.jr9@hotmail.com>
2020-11-09 17:53:09 +0300

    Initial Commit

---
+++ frameworks/frameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  frameworksApp.swift
+//  frameworks
+//
+//  Created by Nawaf B Al sharqi on 09/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct frameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
